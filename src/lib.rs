ddc61864fa0914725b9034cc1bfaa9343804a229
suremarc <8771538+suremarc@users.noreply.github.com>
2023-02-10 23:33:19 -0600

    almost a functioning simulation

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
-use std::time::Duration;
+use std::{pin::Pin, time::Duration};
 
 use async_stream::stream;
 use nohash_hasher::IntSet;
 use rpds::ListSync;
 use tokio::{sync::mpsc, time::Instant};
-use tokio_stream::Stream;
+use tokio_stream::{Stream, StreamExt, StreamMap};
 
+#[derive(Clone)]
 pub enum Message {
     Block(Blockchain),
     Txn(u64),
 }
 
+// TODO: actually add real currency to the chain
+#[derive(Debug, Clone)]
 pub struct Block {
     pub txns: IntSet<u64>,
 }
@@ -28,7 +31,17 @@ pub struct Node {
 }
 
 impl Node {
-    pub fn run(&mut self) -> impl Stream + '_ {
+    pub fn new(rx: mpsc::Receiver<Message>) -> Self {
+        Self {
+            rx,
+            txn_pool: Default::default(),
+            current_block: Default::default(),
+        }
+    }
+}
+
+impl Node {
+    pub fn run(&mut self) -> impl Stream<Item = Message> + '_ {
         stream! {
             const T_NEXT_BLOCK: Duration = Duration::from_secs(1);
 
@@ -47,6 +60,7 @@ impl Node {
                     },
                     msg = self.rx.recv() => match msg {
                         // We received a message.
+                        // TODO: verify that every transaction is valid and has not been double-spent.
                         Some(Message::Block(block)) => {
                             if self.current_block.len() < block.len() {
                                 // A longer chain has been received.
@@ -57,9 +71,8 @@ impl Node {
                         },
                         Some(Message::Txn(id)) => {
                             // For now just broadcast it blindly.
-                            // TODO: verify that the transaction is valid and
-                            // has not been double-spent.
-                            if !self.txn_pool.contains(&id) {
+                            // TODO: verify that the transaction is valid and has not been double-spent.
+                            if self.txn_pool.insert(id) {
                                 yield Message::Txn(id);
                             }
                         }
@@ -68,6 +81,36 @@ impl Node {
                     },
                 }
             }
+        }
+    }
+}
+
+pub async fn run_net(buf_size: usize, n: usize) {
+    let (txs, nodes): (Vec<_>, Vec<_>) = (0..n)
+        .map(|_| mpsc::channel::<Message>(buf_size))
+        .map(|(tx, rx)| (tx, Node::new(rx)))
+        .unzip();
+
+    let mut nodes = nodes.into_boxed_slice();
+
+    let streams: Box<[_]> = nodes.iter_mut().map(Node::run).collect();
+    tokio::pin!(streams);
+
+    let mut stream_map: StreamMap<_, _> = streams
+        .iter_mut()
+        // SAFETY: 'streams' is pinned, so every stream inside 'streams' is also pinned.
+        .map(|stream| unsafe { Pin::new_unchecked(stream) })
+        .enumerate()
+        .collect();
+
+    while let Some((stream_idx, ref msg)) = stream_map.next().await {
+        match msg {
+            Message::Block(blk) => println!("{:?}", blk.first()),
+            Message::Txn(id) => println!("transaction: {id}"),
+        };
+
+        for (_, tx) in txs.iter().enumerate().filter(|&(i, _)| i == stream_idx) {
+            _ = tx.send_timeout(msg.clone(), Duration::from_micros(0)).await;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use satoshi::run_net;
+
+#[tokio::main]
+async fn main() {
+    run_net(100, 1000).await
 }
